4f748da46e05046bf42609738812d38e149f9030
Esleyder <brayanesleyderrengifocuero09@outlook.com>
2021-03-17 23:35:56 -0500

    css

---
+++ boletines/javascript/main.js
@@ -0,0 +1,33 @@
+//DOM
+//query selector
+/*let links = document.querySelectorAll("a");
+links.forEach(function(link){
+  console.log(link);
+});
+*/
+//let celdas = document.querySelectorAll("td");
+
+//celdas.forEach(function(td){
+//	td.addEventListener('click',function(){
+//		console.log(this);
+//	})
+//});
+//obtener los elementos de la clase close
+
+let links = document.querySelectorAll(".close");
+//recorrerlos
+links.forEach(function(link){
+
+	link.addEventListener("click",function(){
+	console.log(":)");
+   });
+});
+
+
+//agregar un evento
+
+
+
+
+
+
