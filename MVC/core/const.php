3b166ce359236b8c20fdebad6085fd821476417f
Esleyder <brayanesleyderrengifocuero09@outlook.com>
2021-03-27 12:51:57 -0500

    ok

--- MVC/core/const.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('DATA_ANIMAL',array('nombre'=>'Nombre','especie'=>'Especie','raza'=>'Raza','color'=>'Color','edad'=>'Edad'));
-
-?>
--- MVC/core/constusuario.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('DATA_USUARIO',array('nombre'=>'Nombre','apellido'=>'Apellido','sexo'=>'Sexo','direccion'=>'Direccion','telefono'=>'Telefono','edad'=>'Edad'));
-
-?>
