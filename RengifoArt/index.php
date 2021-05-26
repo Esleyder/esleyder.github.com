2dfce7985c684776bb2d55e5450a82fed28c8380
Esleyder <brayanesleyderrengifocuero09@outlook.com>
2021-05-25 19:14:09 -0500

    index

---
+++ RengifoArt/index.php
@@ -0,0 +1,21 @@
+<?php
+
+     require_once './views/Partials/head.php';
+
+?>
+<?php
+
+     require_once './views/Partials/header.php';
+
+?>
+<?php
+
+     require_once './views/Partials/menu.php';
+
+?>
+<?php
+
+     require_once './views/Partials/footer.php';
+
+?>
+         
