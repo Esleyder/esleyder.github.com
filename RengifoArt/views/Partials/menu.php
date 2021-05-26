3e7bbd2a1004f2a600462fc4c3aa7248d0fd34ff
Esleyder <brayanesleyderrengifocuero09@outlook.com>
2021-05-25 19:03:25 -0500

    subir

---
+++ RengifoArt/views/Partials/menu.php
@@ -0,0 +1,91 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<a href="https://api.whatsapp.com/send?phone=573206489279&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202." class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+ <div class="page-content">
+      <div>
+<div class="da-section">
+  <div class="container">
+    <div class="h3 text-center" data-aos="fade-up">Principal</div>
+  </div>
+  <div class="da-team carousel slide py-5" id="da-carouselIndicator" data-ride="carousel" data-aos="zoom-in-up" data-aos-duration="1000">
+    <div class="container">
+      <ol class="carousel-indicators">
+        <li class="active" data-target="#da-carouselIndicator" data-slide-to="0"></li>
+        <li data-target="#da-carouselIndicator" data-slide-to="1"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/1.jpg" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/2.jpg" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/3.jpg" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/4.JPG" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/5.JPG" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+          <div class="col-md-4">
+              <div class="card d-block mb-3"><img class="card-img-top" src="./web/images/personal/6.JPG" alt="image"/>
+                <div class="card-body text-center">
+                  <div class="h5"></div>
+                  <p class="text-muted"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="da-section">
+  <div class="container da-company-brand py-5">
+    <div class="h3 pb-3 text-uppercase text-center" data-aos="fade-up">Vinculado</div>
+    <p class="pb-3 text-center" data-aos="fade-up">he tenido la maravillosa oportunidad de trabajr con estas entidades culturales que apoyan el arte y cultura</p>
+    <div class="row">
+      <div class="col-md-3 text-center"><img class="img-fluid" src="./web/images/company-1.jpg" alt="Company Image 1"/></div>
+      <div class="col-md-3 text-center"><img class="img-fluid" src="./web/images/company-2.jpg" alt="Company Image 2"/></div>
+      <div class="col-md-3 text-center"><img class="img-fluid" src="./web/images/company-3.jpg" alt="Company Image 3"/></div>
+      <div class="col-md-3 text-center"><img class="img-fluid" src="./web/images/company-4.jpg" alt="Company Image 4"/></div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
