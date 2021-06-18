bc8ec3748f506a6a7c145407d6ce50037793c8a1
Esleyder <53105791+Esleyder@users.noreply.github.com>
2021-06-18 12:07:11 -0500

    subir de nuevo proyecto
    lol

---
+++ Rengifoart/views/Partials/footer.php
@@ -0,0 +1,33 @@
+    <footer class="bg-secondary da-section">
+      <div class="container text-white">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="h2 mb-4">RengifoArt</div>
+            <p class="mb-3">rengifoart@esleyder.uno</p>
+          </div>
+          <div class="col-md-4">
+            <div class="h6 pb-2">Sigueme</div>
+            <ul>
+              <li class="mb-1"><a class="da-social-link" href="#"><i class="fab fa-twitter" aria-hidden="true"></i><span class="ml-2">Twitter</span></a></li>
+              <li class="mb-1"><a class="da-social-link" href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i><span class="ml-2">Facebook</span></a></li>
+              <li class="mb-1"><a class="da-social-link" href="#"><i class="fab fa-instagram" aria-hidden="true"></i><span class="ml-2">Instagram</span></a></li>
+              <li class="mb-1"><a class="da-social-link" href="#"><i class="fab fa-youtube" aria-hidden="true"></i><span class="ml-2">Youtube</span></a></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <div class="h6 pb-4">Copyright</div>
+            <p class="pb-1">&copy; Esleyder</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <div id="scrolltop">
+      <button class="btn btn-primary"><span class="icon"><i class="fas fa-angle-up fa-2x"></i></span></button>
+    </div>
+    <script src="./web/scripts/jquery.min.js?ver=1.2.0"></script>
+    <script src="./web/scripts/bootstrap.bundle.min.js?ver=1.2.0"></script>
+    <script src="./web/scripts/aos.js?ver=1.2.0"></script>
+    <script src="./web/scripts/ekko-lightbox.min.js?ver=1.2.0"></script>
+    <script src="./web/scripts/main.js?ver=1.2.0"></script>
+  </body>
+</html>
